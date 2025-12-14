857f987889dc9a225ba0f930038037fbd0525ee6
Kerry McKenney <kerry.mcke@gmail.com>
2025-12-13 20:26:34 -0500

    Display all results

--- app.js
+++ app.js
@@ -1,30 +1,61 @@
+const resultList = document.getElementById('resultList');
+let allResults = [];
 
 
 
 
 
-async function getData() {
+const getData = async () => {
   const url = "https://services1.arcgis.com/79kfd2K6fskCAkyg/arcgis/rest/services/FoodServiceData/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json";
   try {
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error(`Response status: ${response.status}`);
-    }
-    return response;
-  } catch (error) {
-    console.error(error.message);
+    const result = await fetch(url);
+    allResults = await result.json();
+    displayResults(allResults);
+  } catch (err) {
+      console.error(err);
   }
-}
-
-var allResults;
-getData()
-    .then(response => response.json())
-    .then(response => {
-        allResults = response;
-        console.log(allResults)
-});
+};
 
 
+
+const displayResults = (results) => {
+  const htmlString = results.features
+    .map((result) => {
+      return `
+      <li class="result">
+        <h3>${result.attributes.NameSearch}</h3>
+        <p>Grade: ${result.attributes.Grade}</p>
+      </li>
+    `;
+    })
+    .join('');
+  resultList.innerHTML = htmlString;
+};
+
+getData();
+
+
+
+
+
+// async function getData() {
+//   const url = "https://services1.arcgis.com/79kfd2K6fskCAkyg/arcgis/rest/services/FoodServiceData/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json";
+//   try {
+//     const response = await fetch(url);
+//     if (!response.ok) {
+//       throw new Error(`Response status: ${response.status}`);
+//     }
+//     return response;
+//   } catch (error) {
+//     console.error(error.message);
+//   }
+// }
+
+// getData()
+//     .then(response => response.json())
+//     .then(response => {
+//         allResults = response;
+// });
 
 
 
