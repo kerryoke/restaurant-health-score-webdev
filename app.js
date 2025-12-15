add643c3d1a5ed81c15839d783f5ec19aab6913a
Kerry McKenney <kerry.mcke@gmail.com>
2025-12-14 20:02:16 -0500

    Add working search bar

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const resultList = document.getElementById('resultList');
+const searchBar = document.getElementById('searchBar');
 let allResults = [];
 
 
 
+
+
+searchBar.addEventListener('keyup', (e) => {
+  const searchString = e.target.value.toLowerCase();
+
+  const filteredResults = allResults.features.filter((result) => {
+    return result.attributes.EstablishmentName.toLowerCase().includes(searchString);
+  });
+
+  displayResults(filteredResults);
+})
 
 
 const getData = async () => {
@@ -10,21 +22,27 @@ const getData = async () => {
   try {
     const result = await fetch(url);
     allResults = await result.json();
-    displayResults(allResults);
+    displayResults(allResults.features);
   } catch (err) {
       console.error(err);
   }
 };
 
 
+// limit results to regular inspections only: (not complaints, follow-ups, etc);
+// const regularInspections = 
+
+
 
 const displayResults = (results) => {
-  const htmlString = results.features
+  const htmlString = results
     .map((result) => {
       return `
       <li class="result">
-        <h3>${result.attributes.NameSearch}</h3>
+        <h3>${result.attributes.EstablishmentName}</h3>
+        <p>Location: ${result.attributes.Address}</p>
         <p>Grade: ${result.attributes.Grade}</p>
+        <p>Score: ${result.attributes.score}</p>
       </li>
     `;
     })
@@ -35,30 +53,4 @@ const displayResults = (results) => {
 getData();
 
 
-
-
-
-// async function getData() {
-//   const url = "https://services1.arcgis.com/79kfd2K6fskCAkyg/arcgis/rest/services/FoodServiceData/FeatureServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=json";
-//   try {
-//     const response = await fetch(url);
-//     if (!response.ok) {
-//       throw new Error(`Response status: ${response.status}`);
-//     }
-//     return response;
-//   } catch (error) {
-//     console.error(error.message);
-//   }
-// }
-
-// getData()
-//     .then(response => response.json())
-//     .then(response => {
-//         allResults = response;
-// });
-
-
-
-
-// media queries
 
